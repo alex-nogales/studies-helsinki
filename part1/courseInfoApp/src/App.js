4c147ff6e9ed870ae175814f4947f6c2c5dc4e99
Alex Nogales <a.nogales.cs@gmail.com>
2021-03-23 12:04:40 -0300

    Define Part component

--- part1/courseInfoApp/src/App.js
+++ part1/courseInfoApp/src/App.js
@@ -9,12 +9,22 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  return ( 
+  return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
-  </div>
+      <Part part1 = {props.part1} exercises1 = {props.exercises1}/>
+      <Part part2 = {props.part2} exercises2 = {props.exercises2}/>
+      <Part part3 = {props.part3} exercises3 = {props.exercises3}/>
+    </div>
+  )
+}
+
+const Part = (parts) => {
+  return (
+    <div>
+      <p>{parts.part1} {parts.exercises1}</p>
+      <p>{parts.part2} {parts.exercises2}</p>
+      <p>{parts.part3} {parts.exercises3}</p>
+    </div>
   )
 }
 
