5375723b1aeab2b9c8b9ec3a00d7925aec252b0a
Alex Nogales <a.nogales.cs@gmail.com>
2021-04-05 16:21:18 -0400

    Finish datacountries ex 2.14

--- part2/datacountries/src/App.js
+++ part2/datacountries/src/App.js
@@ -7,40 +7,72 @@ const Filter = ( {countrySearch, setCountrySearch} ) => {
   )
 }
 
-const CountryData = ( {country, countrySearch} ) => {
-  const output = country.filter(value => value.name.toLowerCase().includes(countrySearch.toLowerCase()))
+const CountryDisplay = ({ filter }) => {
+  return (
+    filter.map(value =>
+      <div key={value.numericCode}>
+       <h1>{value.name}</h1> 
+       <p>Capital: {value.capital}</p>
+        <p>Population: {value.population}</p>
+       <h2> Languages </h2>
+       {value.languages.map(lang => <li key={lang.iso639_2}>{lang.name}</li>)}
+       <h2> Flag </h2>
+       <img src={value.flag} alt="Flag of the country you selected" style={{width: 150, height: 150}}/>
+       <Weather country={filter} />
+      </div>
+    ) 
+  ) 
+}
+const Weather = ( {country} ) => {
+  const [ weather, setWeather ] = useState(null)
 
-  if (output.length > 10) {
+  useEffect(() => {
+    const params = {
+      access_key: process.env.REACT_APP_API_KEY,
+      query: country[0].capital
+    }
+
+    axios
+    .get('http://api.weatherstack.com/current', {params})
+    .then(response => setWeather(response.data.current) )
+    .catch(error => console.log('error: ', error))
+  }, [country])
+
+  console.log('weather: ', weather)
+
+  return (
+    <div>
+      <h1>Weather in {country[0].capital}</h1>
+      {
+        weather ?
+        <>
+          <p><strong>Temperature: </strong>{weather.temperature}</p>
+          <img src={weather.weather_icons[0]} alt='Weather'/>
+          <p><strong>Wind:</strong> {weather.wind_speed} kmph direction {weather.wind_dir}</p>
+        </>
+        :
+        <p>loading data...</p>
+      }
+    </div>
+  )
+}
+
+const CountryData = ( {country, countrySearch, setCountrySearch} ) => {
+  const filter = country.filter(value => value.name.toLowerCase().includes(countrySearch.toLowerCase()))
+
+  if (filter.length > 10) {
     return (
       <p>Too many matches</p>
     )
-  } else if (output.length === 1) {
+  } else if (filter.length === 1) {
     return (
-      output.map(value =>
-        <div key={value.numericCode}>
-         <h1>
-            {value.name}
-         </h1> 
-         <p>
-           Capital: {value.capital}<br />
-           Population: {value.population}
-         </p>
-         <h2>
-           Languages
-         </h2>
-         {value.languages.map(lang => <li key={lang.iso639_2}>{lang.name}</li>)}
-         <h2>
-           Flag
-         </h2>
-         <img src={value.flag} alt="Flag of the country you selected" />
-        </div>
-      ) 
+      CountryDisplay({filter})
     )
   } else {
     return (
-      output.map((value, index) =>
+      filter.map((value, index) =>
         <div key={index}>
-        <>{value.name} <br /></>
+          <>{value.name} </> <button onClick={() => setCountrySearch(value.name)}>show</button><br />
         </div>
       )
     )
@@ -62,11 +94,13 @@ const App = () => {
   }
   useEffect(fetchData, [])
 
+  //console.log(process.env.REACT_APP_API_KEY)
+
   return (
     <div>
       <h1>Country Search!</h1>
       <Filter countrySearch={countrySearch} setCountrySearch={setCountrySearch} />
-      <CountryData country={country} countrySearch={countrySearch} />
+      <CountryData country={country} countrySearch={countrySearch} setCountrySearch={setCountrySearch} />
     </div>
   )
 }
